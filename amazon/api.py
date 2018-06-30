9bb9f484eed994e0d7145e0cb597934781d0aa50
100rabhgajrey <gajreysourabh@gmail.com>
2018-06-30 06:10:57 -0400

    adding user and admin templates

--- amazon/api.py
+++ amazon/api.py
@@ -1,56 +1,77 @@
-from flask import request, render_template,send_from_directory
-from pymongo import MongoClient
-from amazon import app
+from flask import request, send_from_directory, jsonify, render_template
 
-# connect to mongodb server
-client = MongoClient('localhost', 27017)
-# select pymlb2-amazon database
-db = client['pymlb2-amazon']
+from amazon import app
+from amazon.models import product as product_model
+from amazon.models import user as user_model
 
 
 @app.route('/', methods=['GET'])
-def index():
-    return send_from_directory('./amazon/static', 'index.html')
+def index_page():
+    return render_template('index.html',message='welcome')
 
 
 @app.route('/api/product', methods=['GET', 'POST'])
 def product():
     if request.method == 'GET':
-        # lets search for the product here
-        query = {'name': request.args['name']}
-        matching_products = db['products'].find(query)
+        query = request.args['name']
+        matching_products = product_model.search_product(query)
+        return render_template('results.html', query=query, results=matching_products)
 
-        # return the first matching product
-        return render_template('results.html',
-                               query=query['name'],
-                               results=list(matching_products))
     elif request.method == 'POST':
-        # lets add and update here
         op_type = request.form['op_type']
+        name = request.form['name']
+        price = request.form['price']
+        desc = request.form['desc']
 
-        # read data from request and store in a dict
         prod = {
-            'name': request.form['name'],
-            'desc': request.form['desc'],
-            'price': request.form['price']
+            'name': name,
+            'price': price,
+            'desc': desc
         }
 
-        if op_type == 'add':  # add the product here
-            # insert to DB
-            db['products'].insert_one(prod)
+        if op_type == 'add':
 
-            # take user back to index page
-            return send_from_directory('./amazon/static', 'index.html')
+            product_model.add_product(prod)
+            return 'Product ' + name + ' added successfully!'
 
-        elif op_type == 'update':  # update the product here
-            # create filter and update dicts
-            filter = {'name': request.form['name']}
-            update = {
-                '$set': prod
-            }
+        elif op_type == 'update':
 
-            # update in DB
-            db['products'].update_one(filter=filter, update=update)
+            name = request.form['name']
+            updated_product = {'name': name,
+                               'desc': request.form['desc'],
+                               'price': request.form['price']
+                               }
+            product_model.update_product(name, updated_product)
 
-            # take user back to index page
-            return send_from_directory('./amazon/static', 'index.html')
+            return 'Product details updated successfully!'
+
+        return 'Product not found'
+
+
+@app.route('/api/users', methods=['POST'])
+#Login or Signup
+def user():
+    op_type = request.form['op_type']
+    if op_type == 'login':
+        username = request.form['username']
+        password = request.form['password']
+        success = user_model.authenticate(username, password)
+        if success:
+            if username == 'admin':
+                return render_template('admin.html', name=username)
+            else:
+                user = user_model.search_a_user(username)
+                return render_template('home.html', name=user['name'])
+        else:
+            return render_template('index.html', message='Invalid username/password')
+
+    if op_type == 'signup':
+        name = request.form['name']
+        username = request.form['username']
+        password = request.form['password']
+        existing_user = user_model.search_a_user(username)
+        if existing_user is None:
+            user_model.user_signup(name, username, password)
+            return render_template('home.html', name=name)
+        else:
+            return render_template('index.html', message='username exists')
